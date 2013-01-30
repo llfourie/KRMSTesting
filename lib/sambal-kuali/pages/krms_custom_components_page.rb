997738027885edfc1b3213068ab61986d689423a
llfourie <20670494@nwu.ac.za>
2013-01-30 16:16:19 +0200

    Update 2013-01-30

--- lib/sambal-kuali/pages/krms_custom_components_page.rb
+++ lib/sambal-kuali/pages/krms_custom_components_page.rb
@@ -3,15 +3,17 @@ class CustomComponents < BasePage
   expected_element :course
 
   wrapper_elements
+  frame_element
 
-  element(:course) { |b| b.text_field(id: "KRMS-SingleCourse-SuggestField_control") }
-  element(:gpa) { |b| b.text_field(id: "KRMS-GPA-Field_control") }
-  element(:test_score) { |b| b.text_field(id: "KRMS-TestScore-Field_control") }
-  element(:test_name) { |b| b.text_field(id: "KRMS-TestName-Field_control") }
-  element(:department) { |b| b.text_field(id: "KRMS-Department-Field_control") }
-  element(:organization) { |b| b.text_field(id: "KRMS-Administering-Org-Field_control") }
-  element(:grade_select) { |b| b.select(id: "KRMS-GradeValues-Field_control")}
-  element(:auto_message) { |b| b.a(class: "ui-corner-all")}
-  element(:error_message) { |b| b.img(class: "uif-errorMessageItem-field")}
+  element(:course) { |b| b.frm_temp.text_field(id: "KRMS-SingleCourse-SuggestField_control") }
+  element(:gpa) { |b| b.frm_temp.text_field(id: "KRMS-GPA-Field_control") }
+  element(:test_score) { |b| b.frm_temp.text_field(id: "KRMS-TestScore-Field_control") }
+  element(:test_name) { |b| b.frm_temp.text_field(id: "KRMS-TestName-Field_control") }
+  element(:department) { |b| b.frm_temp.text_field(id: "KRMS-Department-Field_control") }
+  element(:organization) { |b| b.frm_temp.text_field(id: "KRMS-Administering-Org-Field_control") }
+  element(:grade_select) { |b| b.frm_temp.select(id: "KRMS-GradeValues-Field_control")}
+  element(:auto_message) { |b| b.frm_temp.a(class: "ui-corner-all")}
+  element(:error_message) { |b| b.frm_temp.img(class: "uif-errorMessageItem-field")}
+  element(:radio) { |b| b.frm_temp.fieldset(id: "KRMS-GradeScale-Field_fieldset")}
 
 end
